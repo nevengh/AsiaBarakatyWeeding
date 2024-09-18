a8e06bebc3b666eacdcaf5a19c09739fa4407c1b
nevengh <56957050+nevengh@users.noreply.github.com>
2024-09-18 01:53:53 -1100

    edit album1

--- src/Pages/AlbumPhotos/AlbumPhotos.jsx
+++ src/Pages/AlbumPhotos/AlbumPhotos.jsx
@@ -1,32 +1,63 @@
 
 import './AlbumPhotos.css'
 import Hero from '../../components/Hero/Hero'
-import Img1 from './../../assets/Albums/1.jpg'
-import Img2 from './../../assets/Albums/2.jpg'
-import Img3 from './../../assets/Albums/3.jpg'
-import Img4 from './../../assets/Albums/4.jpg'
-import Img5 from './../../assets/Albums/5.jpg'
-import Img6 from './../../assets/Albums/6.jpg'
-import Img7 from './../../assets/Albums/7.jpg'
-import Img8 from './../../assets/Albums/8.jpg'
-import Img9 from './../../assets/Albums/9.jpg'
-import Img10 from './../../assets/Albums/10.jpg'
-import Img11 from './../../assets/Albums/11.jpg'
-import Img12 from './../../assets/Albums/12.jpg'
-import Img13 from './../../assets/Albums/13.jpg'
-import Img14 from './../../assets/Albums/14.jpg'
-import Img15 from './../../assets/Albums/15.jpg'
-import Img16 from './../../assets/Albums/16.jpg'
-import Img17 from './../../assets/Albums/17.jpg'
-import Img18 from './../../assets/Albums/IMG_0636.jpg'
-import Img19 from './../../assets/Albums/19.jpg'
-import Img20 from './../../assets/Albums/20.jpg'
-import Img21 from './../../assets/Albums/21.jpg'
-import Img22 from './../../assets/Albums/22.jpg'
-import Img23 from './../../assets/Albums/23.jpg'
-import Img24 from './../../assets/Albums/24.jpg'
-import Img25 from './../../assets/Albums/25.jpg'
-import Img26 from './../../assets/Albums/26.jpg'
+import Img1 from './../../assets/Album1/1a.jpg'
+import Img2 from './../../assets/Album1/2a.jpg'
+import Img3 from './../../assets/Album1/3a.jpg'
+
+import Img5 from './../../assets/Album1/4a.jpg'
+import Img6 from './../../assets/Album1/5a.jpg'
+
+import Img7 from './../../assets/Album1/7a.jpg'
+import Img8 from './../../assets/Album1/8a.jpg'
+import Img9 from './../../assets/Album1/9a.jpg'
+
+import Img10 from './../../assets/Album1/10a.jpg'
+import Img11 from './../../assets/Album1/11a.jpg'
+import Img12 from './../../assets/Album1/12a.jpg'
+
+import Img13 from './../../assets/Album1/13a.jpg'
+import Img14 from './../../assets/Album1/14a.jpg'
+import Img15 from './../../assets/Album1/15a.jpg'
+
+import Img16 from './../../assets/Album1/16a.jpg'
+import Img17 from './../../assets/Album1/17a.jpg'
+import Img19 from './../../assets/Album1/18a.jpg'
+
+import Img20 from './../../assets/Album1/19a.jpg'
+import Img21 from './../../assets/Album1/20a.jpg'
+import Img22 from './../../assets/Album1/21a.jpg'
+
+import Img23 from './../../assets/Album1/22a.jpg'
+import Img24 from './../../assets/Album1/23a.jpg'
+import Img25 from './../../assets/Album1/24a.jpg'
+
+import Img26 from './../../assets/Album1/25a.jpg'
+import Img27 from './../../assets/Album1/26a.jpg'
+import Img28 from '../../assets/Album1/27a.jpg'
+
+import Img29 from '../../assets/Album1/28a.jpg'
+import Img30 from '../../assets/Album1/29a.jpg'
+import Img31 from '../../assets/Album1/30a.jpg'
+
+import Img32 from '../../assets/Album1/31a.jpg'
+import Img33 from '../../assets/Album1/32a.jpg'
+import Img34 from '../../assets/Album1/33a.jpg'
+
+import Img35 from '../../assets/Album1/34a.jpg'
+import Img36 from '../../assets/Album1/35a.jpg'
+import Img37 from '../../assets/Album1/36a.jpg'
+
+import Img38 from '../../assets/Album1/37a.jpg'
+import Img39 from '../../assets/Album1/38a.jpg'
+import Img40 from '../../assets/Album1/39a.jpg'
+import Img41 from '../../assets/Album1/40.jpg'
+import Img42 from '../../assets/Album1/41.jpg'
+import Img43 from '../../assets/Album1/42.jpg'
+import Img4 from './../../assets/Album1/43.jpg'
+import Img44 from '../../assets/Album1/44.jpg'
+import Img45 from '../../assets/Album1/45.jpg'
+import Img46 from '../../assets/Album1/46.jpg'
 import SeoComponnent from '../../components/SeoComponnent/SeoComponnent'
 
 const AlbumPhotos = () => {
@@ -49,56 +80,95 @@ const AlbumPhotos = () => {
                         <img src={Img2} alt="Image Wedding" />
                         <img src={Img3} alt="Image Wedding" />
                     </div>
-
                     <div className='HJ_groupImgs'>
-                        <img src={Img4} alt="Image Wedding" />
+                        <img src={Img15} alt="Image Wedding" />
+                        <img src={Img37} alt="Image Wedding" />
+                        <img src={Img39} alt="Image Wedding" />
+                    </div>
+
+                    <div className='HJ_groupImgs'>                     
+                        <img src={Img43} alt="Image Wedding" />
                         <img src={Img5} alt="Image Wedding" />
-                        <img src={Img6} alt="Image Wedding" />
-                    </div>
-
-                    <div className='HJ_groupImgs'>
                         <img src={Img7} alt="Image Wedding" />
-                        <img src={Img8} alt="Image Wedding" />
-                        <img src={Img9} alt="Image Wedding" />
                     </div>
 
                     <div className='HJ_groupImgs'>
-                        <img src={Img10} alt="Image Wedding" />
+                        <img src={Img8} alt="Image Wedding" />
                         <img src={Img11} alt="Image Wedding" />
-                        <img src={Img12} alt="Image Wedding" />
+                        <img src={Img10} alt="Image Wedding" />
+                        
                     </div>
-
 
                     <div className='HJ_groupImgs'>
                         <img src={Img13} alt="Image Wedding" />
                         <img src={Img14} alt="Image Wedding" />
-                        <img src={Img15} alt="Image Wedding" />
-                    </div>
-
-
-                    <div className='HJ_groupImgs'>
                         <img src={Img16} alt="Image Wedding" />
-                        <img src={Img17} alt="Image Wedding" />
-                        <img src={Img18} alt="Image Wedding" />
                     </div>
 
+
                     <div className='HJ_groupImgs'>
-                        <img src={Img19} alt="Image Wedding" />
-                        <img src={Img20} alt="Image Wedding" />
+                        
                         <img src={Img21} alt="Image Wedding" />
+                        <img src={Img25} alt="Image Wedding" />
+                        <img src={Img17} alt="Image Wedding" />
+                    </div>
+
+
+                    <div className='HJ_groupImgs'>
+                        <img src={Img26} alt="Image Wedding" />
+                        <img src={Img31} alt="Image Wedding" />
+                        <img src={Img33} alt="Image Wedding" />
+                        
+                        
+                    </div>
+                    
+                    <div className='HJ_groupImgs'>
+                    <img src={Img27} alt="Image Wedding" />
+                        <img src={Img35} alt="Image Wedding" />
+                        <img src={Img36} alt="Image Wedding" />
                     </div>
 
                     <div className='HJ_groupImgs'>
+                        
+                        <img src={Img40} alt="Image Wedding" />
+                        <img src={Img41} alt="Image Wedding" />
+                        <img src={Img42} alt="Image Wedding" />
+                    </div>
+                    <div className='HJ_groupImgs'>
+                        <img src={Img20} alt="Image Wedding" />
                         <img src={Img22} alt="Image Wedding" />
-                        <img src={Img23} alt="Image Wedding" />
                         <img src={Img24} alt="Image Wedding" />
                     </div>
-
+                    
                     <div className='HJ_groupImgs'>
-                        <img src={Img25} alt="Image Wedding" />
-                        <img src={Img26} alt="Image Wedding" />
+                        <img src={Img28} alt="Image Wedding" />
+                        <img src={Img30} alt="Image Wedding" />
+                        <img src={Img34} alt="Image Wedding" />
                     </div>
-
+                    <div className='HJ_groupImgs'>
+                        <img src={Img6} alt="Image Wedding" />
+                        <img src={Img12} alt="Image Wedding" />
+                        <img src={Img44} alt="Image Wedding" />
+                    </div>
+                    <div className='HJ_groupImgs'>
+                        <img src={Img19} alt="Image Wedding" />
+                        
+                        <img src={Img45} alt="Image Wedding" />
+                        
+                        <img src={Img9} alt="Image Wedding" />
+                        
+                    </div>
+                    <div className='HJ_groupImgs'>
+                        <img src={Img38} alt="Image Wedding" />
+                        <img src={Img46} alt="Image Wedding" />
+                        <img src={Img4} alt="Image Wedding" />
+                    </div>
+                    <div className='HJ_groupImgs'>
+                        <img src={Img32} alt="Image Wedding" />
+                        <img src={Img23} alt="Image Wedding" />
+                        <img src={Img29} alt="Image Wedding" />
+                        
+                    </div>
                 </div>
 
             </div>
